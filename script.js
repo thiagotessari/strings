871acd4e576c54904b461d0f18a1192d86fd7a5d
thiagotessari <t.tessari@escola.pr.gov.br>
2025-07-08 14:51:24 +0000

    ok

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
     const palavras = texto.split(" ");
-    campoResultado.textContent = palavras;
+    campoResultado.textContent = palavras.join(", ");
 }
